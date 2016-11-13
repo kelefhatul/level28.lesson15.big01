ec8d5339ff7172a3fe34241708fd1fc3847c2c92
kelefhatul <kostyantinromanov@gmail.com>
2016-11-13 13:21:56 +0200

    taask01

---
+++ big01/Aggregator.java
@@ -0,0 +1,10 @@
+package com.javarush.test.level28.lesson15.big01;
+
+/**
+ * Created by root on 11/13/2016.
+ */
+public class Aggregator {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ big01/model/Strategy.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level28.lesson15.big01.model;
+
+/**
+ * Created by root on 11/13/2016.
+ */
+public interface Strategy {
+}
