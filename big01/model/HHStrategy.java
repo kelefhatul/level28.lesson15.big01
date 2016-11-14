10dc02f51f1ec5fd2f049464d5546b0dc6d222bc
kelefhatul <kostyantinromanov@gmail.com>
2016-11-14 14:29:29 +0200

    task06

--- big01/model/HHStrategy.java
+++ big01/model/HHStrategy.java
@@ -1,7 +1,10 @@
 package com.javarush.test.level28.lesson15.big01.model;
 
 import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,6 +17,13 @@ public class HHStrategy implements Strategy {
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
+        String url = String.format(URL_FORMAT, "Kiev", 0);
+        try {
+            Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0 (jsoup)").timeout(5000).get();
+            System.out.println(doc.html());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return Collections.emptyList();
     }
 }
--- big01/vo/Vacancy.java
+++ big01/vo/Vacancy.java
@@ -58,4 +58,33 @@ public class Vacancy {
     public void setUrl(String url) {
         this.url = url;
     }
+
+    @Override
+    public int hashCode() {
+
+        int result = title !=null ? title.hashCode() : 0;
+        result = 31 * result + (salary != null ? salary.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (companyName != null ? companyName.hashCode() : 0);
+        result = 31 * result + (siteName != null ? siteName.hashCode() : 0);
+        result = 31 * result + (url != null ? url.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj || getClass() == obj.getClass()) return true;
+        if (obj == null) return false;
+
+        Vacancy vacancy = (Vacancy) obj;
+
+        if (title != null ? !title.equals(vacancy.title) : vacancy.title != null ) return false;
+        if (salary != null ? !salary.equals(vacancy.salary) : vacancy.salary != null ) return false;
+        if (city != null ? !city.equals(vacancy.city) : vacancy.city != null ) return false;
+        if (companyName != null ? !companyName.equals(vacancy.companyName) : vacancy.companyName != null ) return false;
+        if (siteName != null ? !siteName.equals(vacancy.siteName) : vacancy.siteName != null ) return false;
+        if (url != null ? !url.equals(vacancy.url) : vacancy.url != null ) return false;
+
+        return true;
+    }
 }
