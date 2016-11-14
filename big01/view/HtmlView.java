0cfca6f6f735e73665a9a1d455319d13cac01a62
kelefhatul <kostyantinromanov@gmail.com>
2016-11-14 19:13:21 +0200

    task13

--- big01/view/HtmlView.java
+++ big01/view/HtmlView.java
@@ -9,10 +9,14 @@ import java.util.List;
  * Created by root on 11/14/2016.
  */
 public class HtmlView implements View {
+    private final String filePath = "./src/"+this.getClass().getPackage().getName().replaceAll("\\.", "/") + "/vacancies.html";
+
+
+
     private Controller controller;
     @Override
     public void update(List<Vacancy> vacancies) {
-
+        System.out.println(filePath);
     }
 
     @Override
@@ -22,5 +26,11 @@ public class HtmlView implements View {
 
     public void userCitySelectEmulationMethod(){
         controller.onCitySelect("Odessa");
+    }
+    private String getUpdatedFileContent(List<Vacancy> vacancies){
+        return "";
+    }
+    private void updateFile(String name){
+
     }
 }
