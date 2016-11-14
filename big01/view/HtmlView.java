29a884a94376cf28be6ba235ab8d95927e6bd8e5
kelefhatul <kostyantinromanov@gmail.com>
2016-11-14 20:32:38 +0200

    task15

--- big01/view/HtmlView.java
+++ big01/view/HtmlView.java
@@ -2,8 +2,13 @@ package com.javarush.test.level28.lesson15.big01.view;
 
 import com.javarush.test.level28.lesson15.big01.Controller;
 import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Template;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
@@ -14,11 +19,12 @@ import java.util.List;
 public class HtmlView implements View {
     private final String filePath = "./src/"+this.getClass().getPackage().getName().replaceAll("\\.", "/") + "/vacancies.html";
 
+    Template template = new Template();
 
     private Controller controller;
     @Override
     public void update(List<Vacancy> vacancies) {
-        System.out.println(filePath);
+        updateFile(getUpdatedFileContent(vacancies));
     }
 
     @Override
@@ -30,7 +36,39 @@ public class HtmlView implements View {
         controller.onCitySelect("Odessa");
     }
     private String getUpdatedFileContent(List<Vacancy> vacancies){
-        return "";
+        String fileData = "";
+        try{
+            //1
+            Document doc = getDocument();
+            Element templateElement = doc.select(".template").first();
+            Element cloneTemplateElement = templateElement.clone();
+            //2
+            cloneTemplateElement.removeAttr("style");
+            cloneTemplateElement.removeClass("template");
+            //3
+            doc.select("tr[class=vacancy]").remove();
+            //4
+            for (Vacancy vacancy : vacancies
+                 ) {
+                Element cloneFinal = cloneTemplateElement.clone();
+                cloneFinal.getElementsByClass("city").first().text(vacancy.getCity());
+                cloneFinal.getElementsByClass("companyName").first().text(vacancy.getCompanyName());
+                cloneFinal.getElementsByClass("salary").first().text(vacancy.getSalary());
+
+                Element link = cloneFinal.getElementsByTag("a").first();
+                link.text(vacancy.getTitle());
+                link.attr("href", vacancy.getUrl());
+
+                templateElement.before(cloneFinal.outerHtml());
+            }
+            fileData = doc.html();
+
+
+        }catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Some exception occurred");
+        }
+        return fileData;
     }
     private void updateFile(String content){
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))){
@@ -38,5 +76,9 @@ public class HtmlView implements View {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    protected Document getDocument() throws IOException{
+        Document html = Jsoup.parse(new File(filePath), "UTF-8");
+        return html;
     }
 }
